4934f2eb3adc24cc513cfd6b83ad20ab6d8598d4
Michel Kim <kim98.michel@gmail.com>
2023-05-28 18:09:29 -0300

    Add Hilt for dependency injection

--- app/src/main/java/br/com/prepvest/ui/main/MainActivity.kt
+++ app/src/main/java/br/com/prepvest/ui/main/MainActivity.kt
@@ -7,7 +7,9 @@ import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import br.com.prepvest.R
 import br.com.prepvest.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
